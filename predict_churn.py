28adbfde03f4513ca1b7a3f054e23bc01630d987
bruffster <79555627+bruffster@users.noreply.github.com>
2022-06-05 16:58:11 -0600

    Updated original files and added PDF printout
    Added the updated versions of the python file and the jupyter notebook. Also aded the pdf printout of the notebook in it's fully run state.

--- predict_churn.py
+++ predict_churn.py
@@ -5,16 +5,16 @@ def load_data(filepath):
     """
     Loads churn data into a DataFrame from a string filepath.
     """
-    df1 = pd.read_csv(filepath, index_col='customerID')
-    return df1
+    df = pd.read_csv(filepath, index_col='customerID')
+    return df
 
 
-def make_predictions(df1):
+def make_predictions(df):
     """
     Uses the pycaret best model to make predictions on data in the df dataframe.
     """
-    model = load_model('CATBOOST')
-    predictions = predict_model(model, data=df1)
+    model = load_model('LR')
+    predictions = predict_model(model, data=df)
     predictions.rename({'Label': 'Churn_prediction'}, axis=1, inplace=True)
     predictions['Churn_prediction'].replace({1: 'Churn', 0: 'No Churn'},
                                             inplace=True)
@@ -22,7 +22,7 @@ def make_predictions(df1):
 
 
 if __name__ == "__main__":
-    df1 = load_data('data/new_churn_data_modified.csv')
-    predictions = make_predictions(df1)
+    df = load_data('data/new_churn_data.csv')
+    predictions = make_predictions(df)
     print('predictions:')
     print(predictions)
